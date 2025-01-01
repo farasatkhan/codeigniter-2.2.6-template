2d66579231fb23fdefa69863da70b65f8fb6919b
Farasat Khan <farasatkahan@gmail.com>
2025-01-01 20:50:16 +0500

    Removed the changes related to migration from clean template

--- application/config/database.php
+++ application/config/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
+$db['default']['username'] = '';
 $db['default']['password'] = '';
-$db['default']['database'] = 'ci2db';
+$db['default']['database'] = '';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
--- application/config/routes.php
+++ application/config/routes.php
@@ -41,7 +41,5 @@
 $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
-$route['migrate'] = 'migrate/index';
-
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
